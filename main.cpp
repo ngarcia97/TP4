a92d8935ba4f37b2e4d275f88dc71e96d1a62118
snir2g2 <snir2g2@localhost>
2018-01-16 17:52:46 +0100

    Ajout du premier cout

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 16 janvier 2018, 17:50
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Ceci est un test";
+    return 0;
+}
+
